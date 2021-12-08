120feec8376f2d3960283734545f6813478d5826
syml30 <86074810+syml30@users.noreply.github.com>
2021-12-08 14:45:15 +0330

    list,detail,add

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from .views import AnnouncementList, AnnouncementDetail, add_announcement_view
 
 urlpatterns = [
+    path('announcement-list/', AnnouncementList.as_view(), name="announcement_list"),
+    path('announcement-detail/<int:pk>/', AnnouncementDetail.as_view(), name="announcement_detail"),
 
+    path('add-announcement-view/', add_announcement_view, name="add_announcement"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,2 +1,36 @@
-from django.shortcuts import render
-from .models import Profile, Category, CategoryAttribute, AnnouncementAttributeValue
+from django.shortcuts import render, redirect, reverse
+from .models import Announcement, Profile, Category, CategoryAttribute, AnnouncementAttributeValue
+from django.views.decorators.http import require_http_methods
+from django.views.generic import ListView, DetailView
+
+
+class AnnouncementList(ListView):
+    model = Announcement
+    queryset = Announcement.objects.all()
+    template_name = "app/announcement_list.html"
+    context_object_name = "announcements_list"
+    paginate_by = 2
+
+
+class AnnouncementDetail(DetailView):
+    model = Announcement
+    queryset = Announcement.objects.all()
+    template_name = "app/announcement_detail.html"
+    context_object_name = "announcements_detail"
+
+
+@require_http_methods(["POST", "GET"])
+def add_announcement_view(request):
+    if request.method=='GET':
+        return render(request,"app/advertisement_registration.html")
+    if request.method == "POST":
+        city = request.POST.get('city')
+        image = request.POST.get('image')
+        mobile = request.POST.get('mobile')
+        title = request.POST.get('title')
+        price = request.POST.get('price')
+        description = request.POST.get('description')
+        # pid = request.POST.get('pid')
+        Announcement.objects.create(city=city, image=image, mobile=mobile, title=title, price=price,
+                                    description=description, user_id=request.user.id)
+    return redirect(reverse("announcement_list"))
